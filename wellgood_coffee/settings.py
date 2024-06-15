61f5c6dda38413807071d1de83f192fb8ebef6c6
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-06-15 13:29:24 +0000

    allauth code update

--- wellgood_coffee/settings.py
+++ wellgood_coffee/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u1!rwiej1awu%58av_18jywio=a4kh0ne(rm0h%p__ez+k(n%_'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,7 +91,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/success'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'wellgood_coffee.wsgi.application'
 
