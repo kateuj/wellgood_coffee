ef66d33923dabcb4fb774551955121fa88388b5f
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-06-28 20:33:40 +0000

    add stripe payment functionality

--- wellgood_coffee/settings.py
+++ wellgood_coffee/settings.py
@@ -18,22 +18,17 @@ if os.path.exists("env.py"):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Stripe payments
-STRIPE_SECRET_KEY = 'your_secret_key'
-STRIPE_PUBLISHABLE_KEY = 'your_publishable_key'
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kateuj-wellgoodcoffee-rbbzlckbe92.ws.codeinstitute-ide.net']
-
+CSRF_TRUSTED_ORIGINS = ['https://8000-kateuj-wellgoodcoffee-rbbzlckbe92.ws.codeinstitute-ide.net']
 
 # Application definition
 
@@ -179,5 +174,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
