262ab04b6cd456f73afc53f792fb392ebb716a21
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-06-30 19:41:48 +0000

    second profile view test fail

--- profiles/tests.py
+++ profiles/tests.py
@@ -6,3 +6,11 @@ class UserProfileModelTest(TestCase):
         user_profiles = UserProfile.objects.count()
 
         self.assertEqual(user_profiles, 0)
+
+    
+class ProfilePageTest(TestCase):
+    def test_profile_page_returns_correct_response(self):
+        response = self.client.get('/')
+
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+        self.assertEqual(response.status_code, 200)
