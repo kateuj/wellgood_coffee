7d78f291c099913758eb782d8185c31a8ac5f0a6
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-06-30 13:48:44 +0000

    add price to product model and finalise stripe payment code

--- products/models.py
+++ products/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     type = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
