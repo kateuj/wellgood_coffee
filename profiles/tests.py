0fc6c2c8327799bc89c6c9e876c2cf4fa826f0cf
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-06-30 19:04:40 +0000

    initial TDD test for profiles models fail

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from .models import Profile
 
-class ProfileTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+class UserProfileModelTest(TestCase):
+    def test_profile_model_exists(self):
+        user_profiles = UserProfile.objects.count()
+
+        self.assertEqual(user_profiles, 0)
