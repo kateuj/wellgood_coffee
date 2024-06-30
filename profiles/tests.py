3d0a0637c5c15c76a12ca813f7de7181c09bf8f4
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-06-30 20:29:44 +0000

    profile page template created. test profile view pass

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from .models import UserProfile
+from django.urls import reverse
 
 class UserProfileModelTest(TestCase):
+
+
     def test_profile_model_exists(self):
         user_profiles = UserProfile.objects.count()
 
@@ -9,8 +12,12 @@ class UserProfileModelTest(TestCase):
 
     
 class ProfilePageTest(TestCase):
-    def test_profile_page_returns_correct_response(self):
-        response = self.client.get('/')
 
-        self.assertTemplateUsed(response, 'profiles/profile.html')
+
+    def test_profile_view_status_code(self):
+        response = self.client.get(reverse('profile'))
         self.assertEqual(response.status_code, 200)
+
+    def test_profile_view_template_used(self):
+        response = self.client.get(reverse('profile'))
+        self.assertTemplateUsed(response, 'profiles/profile.html')
