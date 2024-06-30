632146216b19dd18b0366dc98ac27d86689b62f9
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-06-30 14:17:53 +0000

    initial profiles TDD test run fail

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
