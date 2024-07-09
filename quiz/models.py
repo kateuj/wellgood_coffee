8b502b1d4f1a107d470d08c0dc090bbbc9ac86cc
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-07-09 21:37:49 +0000

    countryfield model update checkout bug fix

--- quiz/models.py
+++ quiz/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Quiz(models.Model):
-    name = models.CharField(max_length=20)
-    topic = models.CharField(max_length=120)
-    number_of_questions = models.IntegerField()
+# class Quiz(models.Model):
+#     name = models.CharField(max_length=20)
+#     topic = models.CharField(max_length=120)
+#     number_of_questions = models.IntegerField()
 
-    def __str__(self):
-        return f"{self.name}-{self.topic}"
+#     def __str__(self):
+#         return f"{self.name}-{self.topic}"
     
-    def get_questions(self):
-        pass
+#     def get_questions(self):
+#         pass
