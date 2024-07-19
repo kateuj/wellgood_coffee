67ab449b116eacd52316ced6a72ce93be25e9b49
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-07-19 21:14:18 +0000

    if statement for when size value is none to not show on detail page

--- products/views.py
+++ products/views.py
@@ -88,7 +88,7 @@ def product_detail(request, product_id):
     context = {
         'product': product,
         'variants': variants,
-        'sizes': [i['size'] for i in sizes ],
+        'sizes': [i['size'] for i in sizes if i['size'] is not None],
         'grinds': [i['grind'] for i in grinds if i['grind'] is not None],
         'variant_prices': [float(i['price']) for i in variant_prices ],
         #'choices': maincategory._meta.get_field('variant').choices
