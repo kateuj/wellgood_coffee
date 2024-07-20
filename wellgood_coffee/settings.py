d558d04e10059aa5dd911af116bd22f41d54db15
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-07-20 20:29:01 +0000

    settings deployment update

--- wellgood_coffee/settings.py
+++ wellgood_coffee/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret_key'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wellgood-coffee-f56fcdb787d6.herokuapp.com', '8000-kateuj-wellgoodcoffee-rbbzlckbe92.ws.codeinstitute-ide.net']
 CSRF_TRUSTED_ORIGINS = ['https://8000-kateuj-wellgoodcoffee-rbbzlckbe92.ws.codeinstitute-ide.net', 'https://wellgood-coffee-f56fcdb787d6.herokuapp.com']
