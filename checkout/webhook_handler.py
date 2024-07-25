e228d73b189e036a11599da10520bebf30eb8eba
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-07-25 19:54:52 +0000

    remove testing print statements

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -21,21 +21,16 @@ class StripeWH_Handler:
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
-        try:
-            subject = render_to_string(
-                "checkout/confirmation_emails/confirmation_email_subject.txt",
-                {"order": order},
-            )
-            body = render_to_string(
-                "checkout/confirmation_emails/confirmation_email_body.txt",
-                {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
-            )
+        subject = render_to_string(
+            "checkout/confirmation_emails/confirmation_email_subject.txt",
+            {"order": order},
+        )
+        body = render_to_string(
+            "checkout/confirmation_emails/confirmation_email_body.txt",
+            {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
+        )
 
-            print(f"Attempting to send email to: {cust_email}")
-
-            send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
-        except Exception as e:
-            print(f"Error sending email: {e}")
+        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
 
     def handle_event(self, event):
         """
@@ -159,7 +154,6 @@ class StripeWH_Handler:
                     status=500,
                 )
         self._send_confirmation_email(order)
-        print("order processed")
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created \
             order in webhook',
