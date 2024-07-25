a84e23ac1e5b60a3b8fd8b873dc371634c4f5238
Kate Ulloa-James <kate.ulloajames@gmail.com>
2024-07-25 19:52:28 +0000

    webhook update for order confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,6 +9,7 @@ from profiles.models import UserProfile
 
 import json
 import time
+import stripe
 
 
 class StripeWH_Handler:
@@ -20,16 +21,21 @@ class StripeWH_Handler:
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
-        subject = render_to_string(
-            "checkout/confirmation_emails/confirmation_email_subject.txt",
-            {"order": order},
-        )
-        body = render_to_string(
-            "checkout/confirmation_emails/confirmation_email_body.txt",
-            {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
-        )
+        try:
+            subject = render_to_string(
+                "checkout/confirmation_emails/confirmation_email_subject.txt",
+                {"order": order},
+            )
+            body = render_to_string(
+                "checkout/confirmation_emails/confirmation_email_body.txt",
+                {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
+            )
 
-        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
+            print(f"Attempting to send email to: {cust_email}")
+
+            send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
+        except Exception as e:
+            print(f"Error sending email: {e}")
 
     def handle_event(self, event):
         """
@@ -48,9 +54,14 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        billing_details = intent.charges.data[0].billing_details
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details # updated
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2) # updated
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
@@ -148,6 +159,7 @@ class StripeWH_Handler:
                     status=500,
                 )
         self._send_confirmation_email(order)
+        print("order processed")
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created \
             order in webhook',
